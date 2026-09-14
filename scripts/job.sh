f4a93a7bdd238c969bf5050aaf156d43dc585765
Semih Cantürk <semih.canturk@umontreal.ca>
2026-09-14 14:09:04 +0300

    fix SBATCH args

--- scripts/job.sh
+++ scripts/job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
-#SBATCH --time=0:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=h100:1
+#SBATCH --cpus-per-task=14
+#SBATCH --mem-per-gpu=64G
 
 # Run the job command passed as an argument when submitting the job ('python main.py' for example)
 echo "Running command: $@"
