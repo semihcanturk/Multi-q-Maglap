90800c6e5e80308cb1153c4804f89e06602a4058
Semih Cantürk <semih.canturk@umontreal.ca>
2026-09-20 00:06:42 +0300

    fixes for CC clusters

--- submit_amp_pe_sweep.sh
+++ submit_amp_pe_sweep.sh
@@ -48,7 +48,7 @@ for entry in "${pe_configs[@]}"; do
       job_name="amp_${target}_${pe_name}_stage${stage}"
       echo "Submitting ${job_name}"
       echo "  ${cmd}"
-      sbatch --job-name="${job_name}" --output="logs/${job_name}_%j.out" wrapper.sb "${cmd}"
+      sbatch --account=def-wolfg --job-name="${job_name}" --output="logs/${job_name}_%j.out" wrapper.sb "${cmd}"
       num_jobs=$((num_jobs + 1))
     done
   done
